cbfc0a76ab9c40b262622721810bdc6b7be8c82d
bismark-bakomora <bismarkzebill@gmail.com>
2024-05-18 14:31:39 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,3 +8,19 @@ function processArray(numbers) {
 
     });
 }
+
+
+// Task 2: Function to format array of strings based on processed numbers
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((string, index) => {
+        if (numbers[index] % 2 === 0) {
+            return string.map.toUpperCase(); // capitalise string if corresponding number is even
+            } else {
+                return string.toLowerCase(); // lowercase string if corresponding number is odd
+            }
+    });
+}
+
+
+module.exports = { processArray, formatArrayStrings};
